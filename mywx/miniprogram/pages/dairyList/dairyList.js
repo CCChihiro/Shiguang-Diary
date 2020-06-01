776575d791ada4e98a962d0e4f4bf598665838f7
RubyZhao <rubyzhao999@gmail.com>
2020-06-01 09:54:00 +0800

    Debug diarylist.

--- mywx/miniprogram/pages/dairyList/dairyList.js
+++ mywx/miniprogram/pages/dairyList/dairyList.js
@@ -6,8 +6,13 @@ Page({
 
   getProperties: function () {
     let that = this
+    var user_id = wx.getStorageSync("user_id")
+    console.log(user_id)
     wx.cloud.callFunction({
       name: "getDiarylist",
+      data: {
+        user_id: user_id
+      },
       success(res){
         console.log("请求云函数成功", res)
         that.setData({
