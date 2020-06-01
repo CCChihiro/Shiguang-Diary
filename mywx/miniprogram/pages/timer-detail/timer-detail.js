02f5e0ba4c5569de219bb6635b5c627c358440e1
ZXY <Xy_zz518@163.com>
2020-06-01 18:38:42 +0800

    新添加的文件内容描述

--- mywx/miniprogram/pages/timer-detail/timer-detail.js
+++ mywx/miniprogram/pages/timer-detail/timer-detail.js
@@ -1,123 +1,243 @@
-
 const app = getApp()
+
 const db = wx.cloud.database();//初始化数据库
 
+
+
 Page({
+
   data: {
+
     nowPgae: 1,
+
     startX: 0,
+
     slider: false,
+
     animationData: {},
+
     showDelete: false,
+
 img_url:'',
+
     title: '', content: '', weather: '', diary_date: '', mood: '', authority: '', times: '', year: '',
+
      background: '',diaryid:'', user_id:''
 
 
+
+
+
   },
+
   touchstart(e) {
+
     this.setData({
+
       startX: e.changedTouches[0].clientX,
+
     })
+
   },
+
   onLoad: function (options) {
+
     this.setData({
+
       content:app.globalData.content,
+
       diary_date:app.globalData.diary_date,
+
       weather:app.globalData.weather,
+
       mood:app.globalData.mood,
+
       authority:app.globalData.authority,
+
       times:app.globalData.times,
+
       year:app.globalData.year,
+
       img_url:app.globalData.img_url,
+
       background:app.globalData.background, 
+
       title:app.globalData.title,
+
       diaryid:app.globalData.diaryid,
+
   })
+
   },
+
+
 
   /***跳转写日记 */
+
   towriteDiary() {
+
     wx.navigateTo({
+
       url: '../writeDiary/writeDiary',
+
     })
+
   },
+
   /***跳转修改日记 */
+
   toeditDiary() {
+
         app.globalData.content= this.data.content  // 获取goodsList[index].num
+
         app.globalData.diary_date= this.data.diary_date  // 获取goodsList[index].num
+
         app.globalData.mood= this.data.mood  // 获取goodsList[index].num
+
         app.globalData.authority= this.data.authority  // 获取goodsList[index].num
+
         app.globalData.times=this.data.times  // 获取goodsList[index].num
+
         app.globalData.year = this.data.year  // 获取goodsList[index].num
+
         app.globalData.img_url= this.data.img_url  // 获取goodsList[index].num
+
         app.globalData.background= this.data.background  // 获取goodsList[index].num
+
         app.globalData.title= this.data.title  // 获取goodsList[index].num
+
         app.globalData.weather = this.data.weather  // 获取goodsList[index].num
+
         app.globalData.diaryid = this.data.diaryid  // 获取goodsList[index].num
+
     wx.navigateTo({
+
       url: '../editDiary/editDiary',
+
     })
+
   },
+
+
 
   /***删除日记 */
+
   deleteDiary: function (e) {
+
     this.setData({
+
       showDelete: true,
+
     })
+
   },
+
   /**点击返回按钮隐藏 */
+
   del_back: function () {
+
     this.setData({
+
       showDelete: false
+
     })
+
   },
+
   /** 点击确定按钮获取input值并且关闭弹窗 */
+
   del_ok: function () {
+
     this.setData({
+
       showDelete: false,
 
+
+
     })
+
     let that = this
+
     db.collection("Users").where({
+
       openid: getApp().globalData.openid,
+
     }).get({
+
       success(res) {
+
         console.log("请求成功", res)
+
         that.setData({
+
           user_id: res.data[0].id
-        })
-        wx.cloud.callFunction({
-          name: "deleteDiary",
-          data: {
-            user_id: that.data.user_id,
-            diary_id: that.data.diaryid
-          },
-          success(res){
-            console.log("请求云函数成功", res)
-          },
-          fail(err){
-            console.log("请求云函数失败", err)
-          }
-        })
-        wx.cloud.callFunction({
-          name: "deleteProperty",
-          data: {
-            user_id: that.data.user_id,
-            diary_id: that.data.diaryid
-          },
-          success(res){
-            console.log("请求云函数成功", res)
-          },
-          fail(err){
-            console.log("请求云函数失败", err)
-          }
+
         })
 
+        wx.cloud.callFunction({
+
+          name: "deleteDiary",
+
+          data: {
+
+            user_id: that.data.user_id,
+
+            diary_id: that.data.diaryid
+
+          },
+
+          success(res){
+
+            console.log("请求云函数成功", res)
+
+          },
+
+          fail(err){
+
+            console.log("请求云函数失败", err)
+
+          }
+
+        })
+
+        wx.cloud.callFunction({
+
+          name: "deleteProperty",
+
+          data: {
+
+            user_id: that.data.user_id,
+
+            diary_id: that.data.diaryid
+
+          },
+
+          success(res){
+
+            console.log("请求云函数成功", res)
+
+          },
+
+          fail(err){
+
+            console.log("请求云函数失败", err)
+
+          }
+
+        })
+
+
+
       },
+
       fail(err) {
+
         console.log("请求失败", err)
+
       },
+
     })
+
   },
+
 })
--- mywx/miniprogram/pages/writeDiary/writeDiary.js
+++ mywx/miniprogram/pages/writeDiary/writeDiary.js
@@ -16,7 +16,7 @@ Page({
     isChecked1: false,
     timerBool: false,
     content: '这是正文1', diary_date: 1002, weather: '', mood: '', authority: '',
-    times: '', year: '', imgbox: [], title: '', diary_id: ''
+    times: '', year: '', img_url: [], title: '', diary_id: ''
   },
 
   datePickerBindchange: function (e) {
@@ -30,6 +30,9 @@ Page({
     })
   },
   imgDelete1: function (e) {
+    this.setData({
+     img_url: ''
+    });
     wx.cloud.callFunction({
       name: 'login',
       data: {},
@@ -48,10 +51,11 @@ Page({
       }
     })
   },
+  
   // 选择图片 &&&
   addPic1: function (e) {
-    var imgbox = this.data.imgbox;
-    console.log(imgbox)
+    var img_url = this.data.img_url;
+    console.log(img_url)
     var that = this;
 
     wx.chooseImage({
@@ -62,13 +66,13 @@ Page({
         // console.log(res.tempFilePaths)
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
         var tempFilePaths = res.tempFilePaths
-        if (imgbox.length == 0) {
-          imgbox = tempFilePaths
-        } else if (5 > imgbox.length) {
-          imgbox = imgbox.concat(tempFilePaths);
+        if (img_url.length == 0) {
+          img_url = tempFilePaths
+        } else if (5 > img_url.length) {
+          img_url = img_url.concat(tempFilePaths);
         }
         that.setData({
-          imgbox: imgbox
+          img_url: img_url
         });
         wx.showLoading({
           title: '上传中',
@@ -77,389 +81,104 @@ Page({
         // 上传图片
         const cloudPath = "img/" + new Date().getTime() + "-" + Math.floor(Math.random() * 1000)
         wx.cloud.uploadFile({
-
-
-
           cloudPath,
-
-
-
           filePath,
-
-
-
           success: res => {
-
-
-
             that.setData({
-
-
-
               img_url: res.fileID
-
-
-
             })
-
-
-
             console.log('[上传文件] 成功：', res)
-
-
-
           },
-
-
-
           fail: e => {
 
-
-
             console.error('[上传文件] 失败：', e)
-
-
-
             wx.showToast({
-
-
-
               icon: 'none',
-
-
-
               title: '上传失败',
-
-
-
             })
-
-
-
           },
-
-
-
           complete: () => {
-
-
-
             wx.hideLoading()
-
-
-
           }
         })
-
-
-
-
       },
-
-
-
       fail: e => {
-
-
-
         console.error(e)
       }
-
     })
   },
 
 
   //提交表单
-
-
-
   formSubmit: function (e) {
-
-
-
     let that = this
-
-
-
     console.log('form发生了submit事件，携带数据为：', e.detail.value);
-
-
-
-    let { content, diary_date, weather, mood, authority, times, year, imgbox, background, title, details } = e.detail.value;
-
-
-
+    let { content, diary_date, weather, mood, authority, times, year, img_url, background, title, details } = e.detail.value;
     that.setData({
-
-
-
-      title, content, diary_date, weather, mood, authority, times, year, imgbox, background, details
-
-
-
+      title, content, diary_date, weather, mood, authority, times, year, img_url, background, details
     })
-
-
-
     var id = new Date().getTime() + "-" + Math.floor(Math.random() * 1000)
-
-
-
     var my_id = id
-
-
-
     app.globalData.img_url = that.data.img_url
-
-
-
     app.globalData.content = that.data.content  // 获取goodsList[index].num
-
-
-
     app.globalData.diary_date = that.data.diary_date  // 获取goodsList[index].num
-
-
-
     app.globalData.mood = that.data.mood  // 获取goodsList[index].num
-
-
-
     app.globalData.authority = that.data.authority  // 获取goodsList[index].num
-
-
-
     app.globalData.times = that.data.times  // 获取goodsList[index].num
-
-
-
     app.globalData.year = that.data.year  // 获取goodsList[index].num
-
-
-
-    app.globalData.imgbox = that.data.imgbox  // 获取goodsList[index].num
-
-
-
     app.globalData.background = that.data.background  // 获取goodsList[index].num
-
-
-
     app.globalData.title = that.data.title  // 获取goodsList[index].num
-
-
-
     app.globalData.weather = that.data.weather  // 获取goodsList[index].num
-
-
-
     app.globalData.diaryid = my_id
-
-
-
     db.collection("Users").where({
-
-
-
       openid: getApp().globalData.openid,
-
-
-
     }).get({
-
-
-
       success(res) {
-
-
-
         console.log("请求成功", res)
-
-
-
         that.setData({
-
-
-
           user_id: res.data[0].user_id
-
-
-
         })
-
-
-
         wx.cloud.callFunction({
-
-
-
           name: "addDiary",
-
-
-
           data: {
-
-
-
             diary_id: my_id,
-
-
-
             content: that.data.content,
-
-
-
             user_id: that.data.user_id,
-
-
-
             date: that.data.diary_date,
-
-
-
             emotion: that.data.mood,
-
-
-
             is_time: that.data.times,
-
-
-
             permission: that.data.authority,
-
-
-
             weather: that.data.weather,
-
-
-
             year: that.data.year,
-
-
-
             title: that.data.title,
-
-
-
             img_url: that.data.img_url
-
-
-
           },
-
-
-
           success(res) {
-
-
-
             console.log("请求云函数addDiary成功", res)
-
-
-
           },
-
-
-
           fail(err) {
-
-
-
             console.log("请求云函数失败", err)
-
-
-
           }
-
-
-
         })
-
-
-
-
-
-
-
       },
-
-
-
       fail(err) {
-
-
-
         console.log("请求失败", err)
-
-
-
       },
-
-
-
     })
     wx.navigateTo({
-
-
-
       url: '../timer-detail/timer-detail',
-
-
-
     })
-
-
-
   },
-
   //公开/隐私权限
-
-
-
   switch1Change: function (e) {
-
-
-
     console.log(e.detail.value)
-
-
-
     var changedData = {};
-
-
-
     changedData['isChecked1'] = !this.data['isChecked1'];
-
-
-
     this.setData(changedData);
-
-
-
-
-
-
-
-
-
-
-
   },
-
-
 
   //时光机
-
-
-
   switch2Change: function (e) {
-
-
-
     console.log('switch2 发生 change 事件，携带值为', e.detail.value)
-
-
-
   },
-
 })
